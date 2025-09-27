132f5257dd839ab0c2261ac170acf4ef4cf9cc17
beckdl <icefire24@hotmail.com>
2025-09-27 16:39:44 -0600

    Added getLocalStorage function to product.js for fix with adding product to the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,16 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || [];
+  if (!Array.isArray(cart)) {
+    setLocalStorage("so-cart", [cart]);
+    return;
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
