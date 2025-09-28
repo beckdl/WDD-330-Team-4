de08194c8a7b6794ecc4635c587e098729b3ef40
Bailey Stones <sto22018@byui.edu>
2025-09-27 20:10:53 -0600

    Fix addToCart bug and format code

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,22 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const currentCart = getLocalStorage("so-cart") || [];
+  if (!Array.isArray(currentCart)) {
+    setLocalStorage("so-cart", [product]);
+    return;
+  }
+  currentCart.push(product);
+  setLocalStorage("so-cart", currentCart);
 }
-// add to cart button event handler
+// Add to cart button handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
-
-// add listener to Add to Cart button
+// Add to cart button listener
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
