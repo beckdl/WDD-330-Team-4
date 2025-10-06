c98c66aaf3b569b613174f5950782b38808533c8
beckdl <icefire24@hotmail.com>
2025-10-06 15:27:05 -0600

    Readded the addProductToCart function that was somehow removed.

--- src/js/product.js
+++ src/js/product.js
@@ -7,6 +7,16 @@ import productDetails from "./productDetails.mjs";
 const productId = getParam("product");
 productDetails(productId);
 
+function addProductToCart(product) {
+  const currentCart = getLocalStorage("so-cart") || [];
+  if (!Array.isArray(currentCart)) {
+    setLocalStorage("so-cart", [product]);
+    return;
+  }
+  currentCart.push(product);
+  setLocalStorage("so-cart", currentCart);
+}
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
