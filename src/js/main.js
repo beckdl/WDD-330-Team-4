c35783691d37b66f20d5a155b814af80c00a37d6
beckdl <icefire24@hotmail.com>
2025-12-10 13:28:57 -0700

    fixed jumping of page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
-import { loadHeaderFooter, showRegistrationPrompt } from "./utils.mjs";
+import { alertMessage, loadHeaderFooter, showRegistrationPrompt } from "./utils.mjs";
 
 loadHeaderFooter();
 
 showRegistrationPrompt();
+
+const newsLetterBtn = document.querySelector("#newsletter-button");
+newsLetterBtn.addEventListener("click", (e) => {
+  const email = document.querySelector("#email").value;
+  if (!email) {
+    return;
+  }
+  alertMessage("Thank you for subscribing to our newsletter!");
+});
